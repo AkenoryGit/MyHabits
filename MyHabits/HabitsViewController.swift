2a4345dd3e69daba9997a6f068e43a84265ce41c
Dmitriy D <akenory@gmail.com>
2025-06-19 11:59:22 +0500

    Настроен цвет интерфейса

---
+++ MyHabits/HabitsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HabitsViewController.swift
+//  MyHabits
+//
+//  Created by Дмитрий Дудник on 19.06.2025.
+//
+
+import UIKit
+
+class HabitsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = "Сегодня"
+        view.backgroundColor = .systemBackground
+    }
+
+}
